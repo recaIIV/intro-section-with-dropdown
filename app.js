6fa2896fdf73e6c7f3a0df3194e67b39b7199196
recaIIV <116628835+recaIIV@users.noreply.github.com>
2023-01-22 19:45:26 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const ham  = document.querySelector(".hamburger");
+const header  = document.querySelector(".header");
+
+ham.addEventListener("click", function(){
+    header.classList.toggle("active");
+})
+
